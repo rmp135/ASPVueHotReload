96ec8429e2469fe0b7368942725bf1d82f9e50dc
Ryan Poole <rmp135@gmail.com>
2022-02-13 13:13:04 +0000

    Prevent screen from clearing on build.

--- VueHotReload/client/vite.config.js
+++ VueHotReload/client/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  clearScreen: false,
   root: './src',
   server: {
     proxy: {
